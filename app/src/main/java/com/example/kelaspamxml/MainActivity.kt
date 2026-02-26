74a62010aaa158982844570b6bd731a466344b35
ghanibaskara <ghanibaskaraa111@gmail.com>
2026-02-26 09:07:05 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/kelaspamxml/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.kelaspamxml
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ScrollView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(0,0,0,0)
+            insets
+        }
+
+    }
+}
